5f6316830f59fde1577addf25a568a3d3a8b44fa
Aliaume LOPEZ <ad.lopez@uw.edu.pl>
2024-03-05 11:43:59 +0100

    adding nix for reproducible builds

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+  pkgs = import <nixpkgs> {};
+in pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (python-pkgs: [
+      python-pkgs.pandas
+      python-pkgs.requests
+      python-pkgs.panflute
+    ]))
+    pkgs.pandoc
+    pkgs.gnumake
+    pkgs.texliveFull
+    pkgs.inkscape
+    pkgs.git
+    pkgs.bash
+    pkgs.entr
+    pkgs.websocat
+    pkgs.gnutar
+    pkgs.zip
+  ];
+}
