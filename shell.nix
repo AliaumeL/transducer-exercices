16fc0a5931f6256b168d5d8e8330852f813c4e53
Aliaume LOPEZ <ad.lopez@uw.edu.pl>
2024-03-08 14:21:34 +0100

    migrating to nix

--- shell.nix
+++ shell.nix
@@ -1,12 +1,16 @@
 let
   pkgs = import <nixpkgs> {};
 in pkgs.mkShell {
+  shellHook = ''
+    exec fish
+  '';
   packages = [
     (pkgs.python3.withPackages (python-pkgs: [
       python-pkgs.pandas
       python-pkgs.requests
       python-pkgs.panflute
     ]))
+    pkgs.fish
     pkgs.git
     pkgs.nodePackages.mathjax
     pkgs.pandoc
